149a1be33d80d8a3602717f7d34d5a65e44b6bb6
davidmeimoun <david.meimoun@hotmail.fr>
2017-11-20 22:00:40 +0100

    i

---
+++ src/domain/util/ApplicationContextUtils.java
@@ -0,0 +1,26 @@
+package domain.util;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class ApplicationContextUtils {
+	private static ApplicationContext context;
+	
+	public static ApplicationContext getApplicationContext(){
+		if(context == null){
+			context = new ClassPathXmlApplicationContext(new String[]{"../applicationContext.xml"});
+		}
+		return context;
+	}
+	
+	public static ApplicationContext getApplicationContext(ServletContext servletContext){
+		if(context == null){
+			context = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+		}
+		return context;
+	}
+	
+}
