2be5ec7f651bc36754e00a1821312fcfca5d3849
davidmeimoun <david.meimoun@hotmail.fr>
2017-11-26 22:17:04 +0100

    ll

---
+++ src/dao/IDAOContact.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.util.List;
+import java.util.Set;
+
+import domain.Address;
+import domain.Contact;
+import domain.PhoneNumber;
+
+public interface IDAOContact {
+	public Contact addContact(String fname, String lname, String email, Address address, Set<PhoneNumber> phones,
+			int numSiret);
+
+	public boolean deleteContact(long id);
+
+	public void generateContact();
+
+	public List<Contact> listContact();
+
+	public Contact getContact(long id);
+
+	public int getNumSiretEntreprise(long id);
+
+	public Contact updateContact(Contact c, String fname, String lname, String email, Address address,
+			Set<PhoneNumber> profiles, int siretnum);
+
+}
---
+++ src/dao/IDAOContactGroup.java
@@ -0,0 +1,28 @@
+package dao;
+
+import java.util.List;
+
+import domain.Contact;
+import domain.ContactGroup;
+
+public interface IDAOContactGroup {
+
+	public boolean createContactGroup(String name);
+
+	public boolean addContactToGroup(Long idGroupContact, Long idContact);
+
+	public List<ContactGroup> listGroupContact();
+
+	public ContactGroup getContactGroup(long id);
+
+	public List<Contact> listContactInGroup(long idGroup);
+
+	public List<Contact> listContactOutsideGroup(long idGroup);
+
+	public boolean deleteContactFromGroup(long idGroup, long idContact);
+
+	public boolean deleteContactFromAllGroup(long idContact);
+
+	public boolean deleteGroupContact(long idGroup);
+
+}
